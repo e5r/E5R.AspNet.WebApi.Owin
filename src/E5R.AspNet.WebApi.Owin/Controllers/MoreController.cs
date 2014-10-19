749789125df1ea035634de1baee1cd6bfa753405
Erlimar Silva Campos <erlimar@gmail.com>
2014-10-19 16:55:22 -0200

    etep8: Adicionado suporte a Microsoft.AspNet.Mvc; Necesario System.Web e Web.config especifico para /Views

---
+++ src/E5R.AspNet.WebApi.Owin/Controllers/MoreController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace E5R.AspNet.WebApi.Owin.Controllers
+{
+    public class MoreController : Controller
+    {
+        public ActionResult Index(Nullable<int> id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
